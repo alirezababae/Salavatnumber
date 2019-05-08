b66440b86c44d69e29656f9ef8a27ea6bc165537
arb <myalirezababaei@gmail.com>
2019-05-08 18:24:33 +0430

    edit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React  from 'react';
 import {StyleSheet, Text, View,Button,Alert} from 'react-native';
 /*
-/
+/red
 export default class App extends React.Component {
   render() {
     return (
